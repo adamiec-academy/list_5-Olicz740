1652b6a9f564549861f5cd1d877b1cc7fc25ee6a
Olicz740 <115731309+Olicz740@users.noreply.github.com>
2022-12-20 12:45:56 +0100

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,10 @@
 def longest_word():
-    return
+    data = []
+    largestWord = 0
+    for line in open("words.txt", encoding="utf-8"):
+        data.append(line.strip())
+    for word in data:
+        if 28 < len(word):
+            largestWord = word
+    return largestWord
+print(longest_word())
