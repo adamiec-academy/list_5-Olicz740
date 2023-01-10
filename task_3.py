62e97b258ad9f7181d69bc4de42b25c05a245b29
Olicz740 <115731309+Olicz740@users.noreply.github.com>
2023-01-10 13:12:37 +0100

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,15 +1,26 @@
 from turtle import *
+
 offset_x = -500
+
 offset_y = 300
+
 data = []
+
 for i in open("dots.txt"):
     data.append(i.strip().split(" "))
+    
 x0, y0 = int(data[0][0]), int(data[0][1])
+
 penup()
+
 goto(x0 + offset_x, - y0 + offset_y)
+
 pendown()
+
 for x in range(len(data)):
     x1, y1 = int(data[x][0]) + offset_x, -int(data[x][1]) + offset_y
     goto(x1,y1)
+
 update()
+
 exitonclick()
